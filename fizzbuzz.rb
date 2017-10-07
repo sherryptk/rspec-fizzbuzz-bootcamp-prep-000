d274f08c900225dfa4aa36ec881da7b6ae153bb7
Sherry <sherryptk@gmail.com>
2017-10-07 16:24:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,12 @@
 def fizzbuzz(number)
   if number % 3 == 0
      "Fizz"
- else number % 5 == 0
+ elsif number % 5 == 0
    "Buzz"
-
+ elsif number % 3 == 0 && number % 5 == 0
+   "FizzBuzz"
+ else
+   false
 end
 
 # def fizzbuzz
